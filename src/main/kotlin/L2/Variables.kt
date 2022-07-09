b15e1c805914663b318960db7443b0dc805e03d8
MichaelZalogin <zaloginmiha@gmail.com>
2022-07-10 02:21:51 +0300

    Lesson 2. Variables

---
+++ src/main/kotlin/L2/Variables.kt
@@ -0,0 +1,16 @@
+package L2
+
+fun main() {
+    val test1: String = "Hello"
+    var test2: String = "Hello"
+    test2 = "Goodbye"
+    val test3 = "Hello"
+    var test4 = "Hello"
+    var test5 = 4
+    var test6 = 8
+    test2 = "Goodbye"
+
+    println("Value of test $test3")
+    println("Value of test ${2+2}")
+    println("Value of test ${test5+test6}")
+}
